70fe24a6104b0ceb1b6a9116620b75f245d04f5c
Crowdbotics <team@crowdbotics.com>
2022-07-29 04:41:05 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "divine_wood_36484",
+  url: "https://divine_wood_36484.botics.co",
+  api: "https://divine_wood_36484.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
